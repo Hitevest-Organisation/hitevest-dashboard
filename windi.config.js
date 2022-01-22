54a768101ffe564ba93275b31902f977d387ea15
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-01-22 04:02:36 +0100

    WindiCSS Config

--- windi.config.js
+++ windi.config.js
@@ -5,4 +5,18 @@ export default defineConfig({
     include: ["**/*.{jsx,tsx,css}"],
     exclude: ["node_modules", ".git", ".next"],
   },
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#0862a0",
+          light: "#0c8de5",
+        },
+        secondary: "#80cbff19",
+      },
+    },
+  },
 });
