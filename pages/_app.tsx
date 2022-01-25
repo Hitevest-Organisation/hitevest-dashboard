bac1e613c3b2de2dd635527cee7ca77aa3897b1a
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-01-26 00:30:17 +0100

    Installed Poppins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,31 @@
 import "windi.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Hitevent Finance</title>
+        <meta name="description" content="Powered by Today's Solution" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
