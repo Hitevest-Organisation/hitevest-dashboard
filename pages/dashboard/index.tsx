8e92127a03c0b1022521c7924cb7454e90b52e9a
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-01-26 01:28:59 +0100

    Added Dummy Transactions Table

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import DashboardLayout from "../../layouts/DashboardLayout";
+import { transactions } from "../../assets/DummyTransactions";
 
 export default function DashboardHome() {
   const cards = [
@@ -18,7 +19,7 @@ export default function DashboardHome() {
             className="bg-white rounded-lg flex-grow min-w-48 transform duration-300 hover:scale-105"
             key={i}
           >
-            <div className="font-bold p-4 pb-0 text-gray-700">{item}</div>
+            <div className="font-bold p-4 pb-0 text-true-gray-700">{item}</div>
             <div className="flex space-x-4 p-4 pt-1 pb-2 items-center">
               <div className="flex-grow font-medium font-bold text-xl text-[#f2a218]">
                 $ 600k
@@ -39,8 +40,8 @@ export default function DashboardHome() {
         ))}
       </section>
 
-      <section className="p-4 md:p-8">
-        <section className="bg-white rounded-2xl p-4">
+      <section className="p-4 md:p-8 lg:p-20">
+        <section className="bg-white rounded-3xl p-4">
           <div className="flex space-x-4">
             <button className="bg-primary rounded-xl font-bold bg-opacity-20 text-primary text-sm text-center p-2 px-4 capitalize md:px-8 hover:(bg-opacity-100 bg-secondary text-white) ">
               Deposit
@@ -49,14 +50,63 @@ export default function DashboardHome() {
             {["Witdrawal", "Interest"].map((item, i) => (
               <button
                 key={i}
-                className="rounded-xl font-bold text-sm py-2 px-4 text-gray-400 capitalize md:px-8 hover:(text-primary bg-secondary) "
+                className="rounded-xl font-bold text-sm py-2 px-4 text-true-gray-400 capitalize md:px-8 hover:(text-primary bg-secondary) "
               >
                 {item}
               </button>
             ))}
           </div>
 
-          <div className="py-8">fghfjhgjhk</div>
+          <div className="py-4 overflow-x-auto">
+            <section className="rounded w-full table table-auto">
+              <div className="table-header-group">
+                <div className="font-medium text-sm text-true-gray-600 uppercase table-row">
+                  <div className="font-bold text-left p-2 py-4 table-cell">
+                    Date
+                  </div>
+                  <div className="font-bold text-left p-2 table-cell">
+                    <div className="inline md:hidden">Trans ID</div>
+                    <div className="hidden md:inline">Transaction ID</div>
+                  </div>
+                  <div className="font-bold text-left p-2 table-cell">
+                    Amount
+                  </div>
+                  <div className="font-bold text-left p-2 table-cell">
+                    Wallet Ballance
+                  </div>
+                  <div className="font-bold text-left p-2 table-cell">
+                    Remark
+                  </div>
+                </div>
+              </div>
+
+              {transactions.map((transaction, i) => (
+                <div key={i} className="text-true-gray-600 table-row-group">
+                  <div className="text-sm table-row">
+                    <div className="text-xs p-2 table-cell md:(py-4) ">
+                      {transaction.date}
+                    </div>
+                    <div className="p-2 table-cell">{transaction.id}</div>
+                    <div className="p-2 table-cell">
+                      $ {transaction.amount.toLocaleString()}
+                    </div>
+                    <div className="p-2 table-cell">
+                      $ {transaction.balance.toLocaleString()}
+                    </div>
+                    <div className="p-2 table-cell whitespace-nowrap">
+                      {transaction.remark}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </section>
+
+            <nav className="flex pt-4 justify-end">
+              <button className="bg-primary rounded-xl font-bold bg-opacity-20 text-primary text-sm text-center p-2 px-4 capitalize md:px-8 hover:(bg-opacity-100 bg-secondary text-white) ">
+                Veiw All →
+              </button>
+            </nav>
+          </div>
         </section>
       </section>
     </DashboardLayout>
