243831515fc8164962562f1e6581417f8783e0fe
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-01-26 04:28:50 +0100

    Dummy Referrals

--- pages/dashboard/referals.tsx
+++ pages/dashboard/referals.tsx
@@ -7,12 +7,94 @@ export default function ReferalsPage() {
   const [currentTransactionsBtn, setCurrentTransactionsBtn] = useState(0);
   const transactionsBtn = ["deposit", "interest", "Investment"];
 
+  const referrees = [
+    {
+      name: "Lolade Adebayo",
+      date: "1/2/2022",
+      userType: "Starter",
+    },
+    ,
+    {
+      name: "Oguneko Damilola",
+      date: "3/2/2022",
+      userType: "Enterprise",
+    },
+    ,
+    {
+      name: "Olajide Zainab",
+      date: "14/2/2022",
+      userType: "Starter",
+    },
+  ];
+
+  const transactions = [
+    {
+      id: "#abc",
+      date: "1/2/2022",
+      from: "Shade",
+      type: "-",
+      commission: {
+        amount: 110,
+        percent: 10,
+      },
+    },
+    {
+      id: "#qwerty",
+      date: "1/2/2022",
+      from: "Bayo",
+      type: "-",
+      commission: {
+        amount: 3330,
+        percent: 10,
+      },
+    },
+    {
+      id: "#xyz",
+      date: "1/2/2022",
+      from: "cc",
+      type: "-",
+      commission: {
+        amount: 40,
+        percent: 45200,
+      },
+    },
+  ];
+
   return (
     <DashboardLayout alias="referals">
       <section className="p-4 md:p-8">
         <section className="bg-white rounded-3xl p-4 md:(p-8)">
           <Pagetitle>Referred Users</Pagetitle>
-          <TransactionsTable />
+          <section className="py-4 overflow-x-auto">
+            <div className="rounded w-full table table-auto">
+              <div className="table-header-group">
+                <div className="font-medium text-sm text-true-gray-600 uppercase table-row">
+                  <div className="font-bold text-left p-2 py-4 table-cell">
+                    Date
+                  </div>
+                  <div className="font-bold text-left p-2 table-cell">
+                    Full Name
+                  </div>
+                </div>
+              </div>
+
+              {referrees.map((referree, i) => (
+                <div key={i} className="text-true-gray-600 table-row-group">
+                  <div className="text-sm table-row">
+                    <div className="p-2 table-cell whitespace-nowrap">
+                      {referree?.date}
+                    </div>
+                    <div className="p-2 table-cell whitespace-nowrap">
+                      {referree?.name}
+                    </div>
+                    <div className="p-2 table-cell whitespace-nowrap">
+                      {referree?.userType}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
         </section>
       </section>
 
@@ -35,7 +117,48 @@ export default function ReferalsPage() {
             ))}
           </div>
 
-          <TransactionsTable />
+          <section className="py-4 overflow-x-auto">
+            <div className="rounded w-full table table-auto">
+              <div className="table-header-group">
+                <div className="font-medium text-sm text-true-gray-600 uppercase table-row">
+                  <div className="font-bold text-left p-2 py-4 table-cell">
+                    Date
+                  </div>
+                  <div className="font-bold text-left p-2 table-cell">From</div>
+
+                  <div className="font-bold text-left p-2 table-cell">
+                    commission %
+                  </div>
+                  <div className="font-bold text-left p-2 table-cell">
+                    Amount ($)
+                  </div>
+                  <div className="font-bold text-left p-2 table-cell">Type</div>
+                </div>
+              </div>
+
+              {transactions.map((referree, i) => (
+                <div key={i} className="text-true-gray-600 table-row-group">
+                  <div className="text-sm table-row">
+                    <div className="p-2 table-cell whitespace-nowrap">
+                      {referree?.date}
+                    </div>
+                    <div className="p-2 table-cell whitespace-nowrap">
+                      {referree?.from}
+                    </div>
+                    <div className="p-2 table-cell whitespace-nowrap">
+                      {referree?.commission?.percent}
+                    </div>
+                    <div className="p-2 table-cell whitespace-nowrap">
+                      {referree?.commission?.amount.toLocaleString()}
+                    </div>
+                    <div className="p-2 table-cell whitespace-nowrap">
+                      {referree?.type}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
         </section>
       </section>
     </DashboardLayout>
